9b08b4f6126cf28cb52a48fd5adaf4ed76afcf01
Riya Chatterjee <56023007+rrriiiyyyaaa@users.noreply.github.com>
2022-01-17 09:41:51 +0530

    Update asli.java

--- asli.java
+++ asli.java
@@ -12,11 +12,11 @@ public class Main {
 		// Given K trials
 		int K = 5;
 		int i, guess;
- 		System.out.println(orignumber); // (for cheating)
+ 		//System.out.println(orignumber); // (for cheating)
 		System.out.println( "A number is chosen" + " between 1 to 50." + "Guess the number" + " within 5 trials.");
-		//Iterate over K(5) Trials
+		// Iterate over K(5) Trials
 		for (i = 0; i < K; i++) 
-		{
+		 {
 		      int k =5;
 			System.out.println("Guess the number:");
 			// Take input for guessing
@@ -26,68 +26,25 @@ public class Main {
 				System.out.println("Congratulations!" + " You guessed the number." + "You pass in "+ (i+1)+ " trails.");
 				break;
 			}
-            else if (orignumber > guess	&& i != K - 1) 
+			else if (orignumber > guess	&& i != K - 1) 
 			{
 				System.out.println( "The number is " + "greater than " + guess );
 			}
 			else if (orignumber < guess	&& i != K - 1) {
 			    System.out.println("The number is" + " less than " + guess);
 			}
-//            else{
-//			    System.out.println("Random number was "+ orignumber);
-//			}
+			else if(i == 4) {
+		    
+			System.out.println( "You have exhausted " + k +" trials. You loose the game.");
+			System.out.println("The number was " + orignumber);
+		    }
 		}
-    }
-    // 		System.out.println("Want to play again??");
-// 		String answer;
-//         boolean yn;
-        
-//         System.out.println("y/n");
-//         while (true) {
-//         if (answer.equals("y")) 
-//         {
-//             yn = true;
-//             for (i = 0; i < K; i++) 
-//     		{
-//     		      int k =5;
-//     			System.out.println("Guess the number:");
-//     			// Take input for guessing
-//     			guess = sc.nextInt();
-//     			// If the number is guessed
-//     			if (orignumber == guess) {
-//     				System.out.println("Congratulations!" + " You guessed the number." + "You pass in "+ (i+1)+ " trails.");
-//     				break;
-//     			}
-//     			else if (orignumber > guess	&& i != K - 1) 
-//     			{
-//     				System.out.println( "The number is " + "greater than " + guess );
-//     			}
-//     			else if (orignumber < guess	&& i != K - 1) {
-//     			    System.out.println("The number is" + " less than " + guess);
-//     			}
-//     		}
-//         } 
-//     else if (answer.equals("n")) {
-//     yn = false;
-//     break;
-// 		for (int j=0;j<k ;i++)
-// 		{
-// 		    k =5;
-// 		    // If the number is guessed
-// 			if (orignumber == guess) {
-// 				System.out.println("Congratulations!" + " You guessed the number." + "You pass in "+ (i+1)+ " trails.");
-// 				break;
-// 			}
-// 			else if (orignumber > guess	&& i != K - 1) 
-// 			{
-// 				System.out.println( "The number is " + "greater than " + guess );
-// 			}
-// 			else if (orignumber < guess	&& i != K - 1) {
-// 			    System.out.println("The number is" + " less than " + guess);
-// 			}
-// 		}
 		
-
+		
+		
+		
+        
+	}
 	// Driver Code
 	public static void main(String arg[])
 	{
